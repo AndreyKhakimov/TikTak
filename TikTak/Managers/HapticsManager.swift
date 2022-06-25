ef299eef958323276cddfa3ec93f7951c1ea5fbf
AndreyKhakimov <khakimov013@gmail.com>
2022-06-25 14:37:54 +0200

    Added HapticsManager logic, HomeVC scrollable feed

--- TikTak/Managers/HapticsManager.swift
+++ TikTak/Managers/HapticsManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+final class HapticsManager {
+    public static let shared = HapticsManager()
+    
+    private init() {}
+    
+    // Public
+    
+    public func vibrateForSeletion() {
+        DispatchQueue.main.async {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+    }
+    
+    public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
+        DispatchQueue.main.async {
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+    }
+}
